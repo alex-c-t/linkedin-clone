60907cd5a31620008a05fca22eafdaf7a290b574
alex-c-t <alextomy8@gmail.com>
2021-12-10 22:53:45 +0530

    security updates

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ak(*%w(k0)3(gw^!st(z=c-slrd5w!l5^mv3-rit_fio75fjl)'
-
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party
+    'rest_framework',
+
+    # local apps
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +130,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.AllowAny',
+    ]
+}
--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Posts:
+class Posts(models.Model):
     name = models.CharField(max_length=40)
     description = models.CharField(max_length=40)
     message = models.TextField()
-    photoUrl = models.CharField(max_length=40)
+    photoUrl = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'send by: {self.name}'
